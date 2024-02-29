5e978ace89c0cbc1bd28774732be372be66e0794
lmkirvan <kirvan.lewis@gmail.com>
2024-02-28 21:30:46 -0500

    stashing changes before I start over again! learning is fun

---
+++ src/README.md
@@ -0,0 +1,10 @@
+## emlconv 
+
+This program is a utility to convert eml files (single emails) into markdown. 
+
+My usecase is to be able to email myself stuff and semi-automatically convert it to an obsidian note. Also I'm trying to learn a "real" programming language. Here I'm giving rust a shot, but will maybe try to recreate in Go as well? Or Ocaml. 
+
+Can have the body be returned as plain text or html. 
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,94 @@
-use std::path::PathBuf;
-use clap::Parser;
+use std::vec;
+use regex::Regex;
+use clap::{Parser, builder::Str};
+use anyhow::{Context, Result, Ok};
 
-///convert eml to markdown
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// path to eml files 
-    #[arg(short, long)]
-    path: Vec<PathBuf>,
-
-    /// subject keywords to look for
-    #[arg(short, long)]
-    subject: Vec<String>,
+struct Eml {
+    mime_version: String,
+    date: String,
+    subject: String,
+    from: String,
+    to: String,
+    boundary: String, 
+    body: String
 }
 
-fn main() {
-    let args = Args::parse();
-
-    for arg in args.path {
-        println!("Hello {}!", arg.display())
+fn find_matches(content: &str, pattern: &str) {
+    for line in content.lines() {
+        if line.starts_with(pattern) {
+            return line.into( ));
+        }
     }
+}
+
+fn get_boundary(content: &str) {
+    let re = Regex::new(r"boundary=\"(.+)\"").unwrap();
+    
+
+
+
+
+}
+
+
+
+fn handle_contents(content){
+
+    let email: Eml = Eml {
+
+        mime_version = find_matches(content, "MIME-Version:"), 
+        date = find_matches(content, "Date:"),
+        subject = find_matches(content, "Subject:"),
+        from = find_matches(content, "From:"),
+        to = find_matches(content, "To:"),
+
+
+
+
+
+    }
+    
+}
+
+
+
+
+///This converts eml to markdown
+#[derive(Parser)]
+struct Cli {
+    /// the pattern to look for
+    pattern: String,
+    /// the file
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("Could not read file '{}'",
+         args.path.display()))?;
+
+    println!("{}", content);
+
+    // println!("is this the content length ? {x}");   
+
+    let email = Eml {
+        mime_version: handle_line(string, pattern)
+    }
+
+    let mut counter = 0; 
+
+
+    for line in content.lines(){
+        //if line.contains(&args.pattern){
+        //    println!("{}", line);
+       // }
+       println!("{}", line);
+
+       println!("{}", counter);
+       counter = counter + 1; 
+    }   
+
+    Ok(())
+
 }
