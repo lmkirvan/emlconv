1cb2887263bcfa2341415c438a28560d09d04c49
lmkirvan <kirvan.lewis@gmail.com>
2024-03-03 09:44:40 -0500

    basically working stuff. I'm actually figuring it out I guess?

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,32 @@ use regex::Regex;
 use clap::{Parser};
 use anyhow::{Context, Result, Ok};
 
-fn get_boundary(content: &str) -> &str {
-    let re = Regex::new(r"(boundary=)").unwrap();
+fn get_boundary(content: &str) -> String {
+    let re = Regex::new("(boundary=)\"(?<b>[a-z,0-9]+)").unwrap();
 
     let Some(caps) = re.captures(content) else {
         println!("no match!");
         panic!()
     };
-    let res = &caps[0];
-    res
+
+    [ "--", &caps["b"].to_string() ].concat()
 }
+
+fn get_line_by_lead(content: String, lead: &str) -> String {
+    let mut matches = content.lines()
+        .filter(|x| x.starts_with(lead));
+
+    let some_date = matches.next();
+    
+    let result = match some_date {
+        Some(_) => {some_date},
+        None => {Some("Date: Unkown")}, 
+    };
+
+   result.expect("no date somehow").to_string()
+
+}
+
 
 ///This converts eml to markdown
 #[derive(Parser)]
@@ -28,8 +44,10 @@ fn main() -> Result<()> {
         .with_context(|| format!("Could not read file '{}'" , args.path.display()))?;
 
     let boundary = get_boundary(&content);
+    
+    let date: String = get_line_by_lead(content, "Date:");
 
-    println!("the boundary is {}", boundary);
+    println!("the boundary is {} and the line is {}", boundary, date);
     
     Ok(())
 
